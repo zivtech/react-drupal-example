e2804f516fb6594e7f4dff11a460c880b9b038dc
Jody Hamilton <jody@zivtech.com>
2017-07-07 12:13:13 -0400

    Update webpack.config.js

--- favorite/js/webpack.config.js
+++ favorite/js/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.css$/, loader: "style!css" }
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   }
 };
